a7f270e17769f0ece2cd4c25e8738c995a30e383
Fiona Neylon <FionaNeylon2022@u.northwestern.edu>
2024-01-07 16:09:25 -0600

    adding color specific rviz config files

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -20,11 +20,13 @@ def generate_launch_description():
 
         DeclareLaunchArgument("color",
                               default_value="purple",
-                              description="setting the color of the base_link of the robot"),
+                              description="setting the color of the base_link of the robot",
+                              choices=["purple", "red", "green", "blue"]),
 
         Node(
             package="joint_state_publisher",
             executable="joint_state_publisher",
+            namespace=LaunchConfiguration('color'),
             condition=IfCondition(PythonExpression(
                 ["'", LaunchConfiguration('use_jsp'), "' == \'true\' "]))
         ),
@@ -32,19 +34,24 @@ def generate_launch_description():
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            # namespace="blue",
+            namespace=LaunchConfiguration('color'),
             parameters=[
                 {"robot_description":
                  Command([TextSubstitution(text="xacro "),
                           PathJoinSubstitution(
-                              [FindPackageShare("nuturtle_description"), "urdf/turtlebot3_burger.urdf.xacro"]), ' color:=', LaunchConfiguration('color')])}
+                              [FindPackageShare("nuturtle_description"), "urdf/turtlebot3_burger.urdf.xacro"]), ' color:=', LaunchConfiguration('color')])}  # [1]: Used in reference for syntax
 
             ]
+
         ),
 
         Node(
             package="rviz2",
             executable="rviz2",
             name="rviz2",
+            namespace=LaunchConfiguration('color'),
+            # namespace=LaunchConfiguration('color'),
             condition=IfCondition(PythonExpression(
                 ["'", LaunchConfiguration('use_rviz'), "' == \'true\' "])),
             arguments=[
