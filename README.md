111cb1a78adcffc4138a831a7660809cd3f1d735
Fiona Neylon <FionaNeylon2022@u.northwestern.edu>
2024-01-20 18:31:59 -0600

    Teleport service works with red robot and the launch file loads the red robot correctly into the nusim world frame

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 This repository consists of several ROS packages
 - nuturtle_description - package that vizualizes the 4 turtlebot3 burgers based on the turtlebot3 burger model from the turtlebot3 repo. 
 - turtlelib- C++ library that contains the geometry and SE(2) geometry classes and svg classes for vizualization of transformations. 
+- nusim - package that simulates the turtlebot3 burgers in a 2D environment with walls and cylindrical obstacles.
--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -2,11 +2,16 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <sstream>
 #include "std_msgs/msg/u_int64.hpp"
 // #include "std_msgs/msg/empty.hpp"
 #include "std_srvs/srv/empty.hpp" // Include the correct header file
-
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include <rclcpp/rclcpp.hpp>
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "nusim/srv/teleport.hpp"
+
 
 using namespace std::chrono_literals;
 
@@ -18,9 +23,9 @@ public:
   {
 
     RCLCPP_INFO(this->get_logger(), "Starting Nusim!");
-    this->declare_parameter("rate_", 200.0);
+    this->declare_parameter("rate", 200.0);
 
-    rate_ = this->get_parameter("rate_").as_double();
+    rate_ = this->get_parameter("rate").as_double();
     
     
     timer_ = this->create_wall_timer(
@@ -28,16 +33,26 @@ public:
     
     publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
     reset_srv_ = this->create_service<std_srvs::srv::Empty>("~/reset", std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2)); // Use the correct service type
+    tf_broadcaster_= std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    teleport_srv_ = this->create_service<nusim::srv::Teleport>("~/teleport", std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
   }
+
 private:
 
   void timer_callback()
   {
-    RCLCPP_INFO(this->get_logger(), "Hello, world!");
+    // RCLCPP_INFO(this->get_logger(), "Hello, world!");
     // time_ = this->get_clock()->now();
     time_ += 1.0/rate_;
     msg_.data = (time_)*1e3;
     publisher_->publish(msg_);
+
+
+    // geometry_msgs::msg::TransformStamped transformStamped;
+    transformStamped_.header.stamp = this->get_clock()->now();
+    transformStamped_.header.frame_id = "nusim/world";
+    transformStamped_.child_frame_id = "red/base_footprint";
+    tf_broadcaster_->sendTransform(transformStamped_);
   }
 
   void reset_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request,
@@ -47,14 +62,44 @@ private:
     time_ = 0.0;
   }
 
+  void teleport_callback(const std::shared_ptr<nusim::srv::Teleport::Request> request,
+                            std::shared_ptr<nusim::srv::Teleport::Response> response)
+  {
+    RCLCPP_INFO(this->get_logger(), "Teleporting Red Robot!");
+    double x = request->x;
+    double y = request->y;
+    double theta = request->theta;
+    // geometry_msgs::msg::TransformStamped transformStamped;
+    transformStamped_.header.stamp = this->get_clock()->now();
+    transformStamped_.header.frame_id = "nusim/world";
+    transformStamped_.child_frame_id = "red/base_footprint";
+    transformStamped_.transform.translation.x = x;
+    transformStamped_.transform.translation.y = y;
+    transformStamped_.transform.translation.z = 0.0;
+    tf2::Quaternion q;
+    q.setRPY(0, 0, theta);
+    transformStamped_.transform.rotation.x = q.x();
+    transformStamped_.transform.rotation.y = q.y();
+    transformStamped_.transform.rotation.z = q.z();
+    transformStamped_.transform.rotation.w = q.w();
+    tf_broadcaster_->sendTransform(transformStamped_);
+
+  }
+
+  geometry_msgs::msg::TransformStamped transformStamped_;
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
   rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_; // Use the correct service type
-  double rate_;
+  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_srv_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_; 
   std_msgs::msg::UInt64 msg_;
   double time_ = 0.0;
+  double rate_;
 
 };
+
+
 
 int main(int argc, char ** argv)
 {
