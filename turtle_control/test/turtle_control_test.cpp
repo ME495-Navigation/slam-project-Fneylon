5b0425c4092ae0ed3a8455db1471e5cdccd8d28f
Fiona Neylon <FionaNeylon2022@u.northwestern.edu>
2024-02-01 22:19:22 -0600

    checkpoint where cmake builds after installing catch_ros2

---
+++ turtle_control/test/turtle_control_test.cpp
@@ -0,0 +1,16 @@
+#include "catch_ros2/catch_ros2.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_srvs/srv/empty.hpp"
+
+using namespace turtle_control::TurtleControl;
+
+TEST_CASE("TurtleControl", "[turtle_control]")
+{
+//   auto node = rclcpp::Node::make_shared("turtle_control");
+//   auto client = node->create_client<std_srvs::srv::Empty>("~/stop");
+//   auto request = std::make_shared<std_srvs::srv::Empty::Request>();
+//   auto result = client->async_send_request(request);
+//   REQUIRE(result.get()->success);
+
+    REQUIRE(1 == 1);
+}
